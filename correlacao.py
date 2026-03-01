3eae94ad0eb3250f6e28cd76fee241d8f4605782
Caio Vitor Melo de Santana <caiovitormelodesantana15@gmail.com>
2026-03-01 19:52:36 -0300

    style: troca de kernel por mascara

--- correlacao.py
+++ correlacao.py
@@ -1,27 +1,24 @@
 import numpy as np
 
-def correlacao(img, kernel, stride=1, r=1, ativacao="identidade"):
+def correlacao(img, mascara, stride=1, r=1, ativacao="identidade"):
 
     img = img.astype(np.float64)
-    kernel = np.array(kernel, dtype=np.float64)
+    mascara = np.array(mascara, dtype=np.float64)
 
     altura, largura, canais = img.shape
-    k_altura, k_largura = kernel.shape
+    k_altura, k_largura = mascara.shape
 
-    # Tamanho efetivo do kernel dilatado (Atrous)
     k_altura_dil = (k_altura - 1) * r + 1
     k_largura_dil = (k_largura - 1) * r + 1
 
-    # Sem padding
     out_altura = (altura - k_altura_dil) // stride + 1
     out_largura = (largura - k_largura_dil) // stride + 1
 
     if out_altura <= 0 or out_largura <= 0:
-        raise ValueError("Kernel maior que a imagem para os parâmetros escolhidos.")
+        raise ValueError("Máscara maior que a imagem para os parâmetros escolhidos.")
 
     output = np.zeros((out_altura, out_largura, canais), dtype=np.float64)
 
-    # Percorrer cada canal separadamente
     for c in range(canais):
 
         for i in range(out_altura):
@@ -35,9 +32,8 @@ def correlacao(img, kernel, stride=1, r=1, ativacao="identidade"):
                         i_img = i * stride + m * r
                         j_img = j * stride + n * r
 
-                        soma += img[i_img, j_img, c] * kernel[m, n]
+                        soma += img[i_img, j_img, c] * mascara[m, n]
 
-                # Aplicação da função de ativação
                 if ativacao == "relu":
                     soma = max(0, soma)
                 elif ativacao == "identidade":
