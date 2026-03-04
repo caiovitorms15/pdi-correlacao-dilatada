2109256d5083934e3e39cdc5159037bef028fafb
Caio Vitor Melo de Santana <caiovitormelodesantana15@gmail.com>
2026-03-04 08:24:56 -0300

    feat: funcao gerar_nome_saida
    função que gera o nome da imagem de saida e coloca na pasta imagens automaticamente

--- funcoesaux.py
+++ funcoesaux.py
@@ -2,6 +2,7 @@ from io_imagem import normalizar_para_uint8
 from correlacao import correlacao
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 def exibir_lado_a_lado(img_original, img_resultado, titulo_original="Original", titulo_resultado="Resultado"):
 
@@ -39,3 +40,8 @@ def aplicar_e_exibir(img, mascara, nome_filtro, nome_imagem, stride=1, r=1, ativ
     
     exibir_lado_a_lado(img, resultado, titulo_original=f"{nome_imagem} - Original", titulo_resultado=titulo)
     return resultado
+
+def gerar_nome_saida(pasta, nome_imagem, nome_filtro, r=1, stride=1, ativacao="identidade"):
+    nome_filtro_limpo = nome_filtro.replace(" ", "_").lower()
+    nome = f"{nome_imagem}_{nome_filtro_limpo}_r{r}_s{stride}_{ativacao}.png"
+    return os.path.join(pasta, nome)
